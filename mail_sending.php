b34fc997323de1e95f61586d7563f99ded170e98
Devadharshini <143801102+deva16081920@users.noreply.github.com>
2024-09-04 12:15:06 +0530

    Add files via upload

--- mail_sending.php
+++ mail_sending.php
@@ -7,11 +7,9 @@ Author: DD
 
 require_once __DIR__ . '/vendor/autoload.php';
 use SendGrid\Mail\Mail;
-
-
+echo 
+'<div class="commona"></div>';
 function email_sender_form() {
-    
-    
 
     if (isset($_POST['send_email'])) {
         
@@ -19,7 +17,11 @@ function email_sender_form() {
         $subject = sanitize_text_field($_POST['subject']);
         $message = sanitize_textarea_field($_POST['message']);
 
-        $sendgrid_api_key = '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';
+        
+        $from_email = 'srissv420@gmail.com';  
+        $from_name = 'Devadharshini';  
+
+        $sendgrid_api_key = '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';
 
         if (!class_exists('\SendGrid\Mail\Mail')) {
             echo '<div class="notice notice-error"><p>SendGrid Mail class not found. Please check if the SendGrid library is properly installed.</p></div>';
@@ -29,13 +31,11 @@ function email_sender_form() {
         $sendgrid = new \SendGrid($sendgrid_api_key);
 
         $email = new Mail();
-        $email->setFrom("your-email@example.com", "Your Name");
+        $email->setFrom($from_email, $from_name); 
         $email->setSubject($subject);
         $email->addTo($to);
         $email->addContent("text/plain", $message);
-        
 
-       
         try {
             $response = $sendgrid->send($email);
             if ($response->statusCode() == 202) {
@@ -48,27 +48,28 @@ function email_sender_form() {
         }
     }
 
+    echo '
+  
 
-    echo'
-    <form method="post" action="">
+    <form method="post" action="" class="common">
+        <p class="heading"> Email Sender </p>
         <p>
-            <label for="to">To:</label>
-            <input type="email" name="to" required />
+            <label for="to">  To:</label>
+            <input type="email" name="to" class="sendgrid-input" required />
         </p>
         <p>
             <label for="subject">Subject:</label>
-            <input type="text" name="subject" required />
+            <input type="text" name="subject" class="sendgrid-input1" required />
         </p>
         <p>
             <label for="message">Message:</label>
-            <textarea name="message" required></textarea>
+            <textarea name="message" class="sendgrid-textarea" required></textarea>
         </p>
         <p>
-            <input type="submit" name="send_email" value="Send Email" />
+            <input type="submit" name="send_email" class="sendgrid-button" value="Send Email" />
         </p>
     </form>';
 }
-
 
 function sender_menu() {
     add_menu_page(
@@ -80,4 +81,9 @@ function sender_menu() {
     );
 }
 add_action('admin_menu', 'sender_menu');
+$plugin_url = plugin_dir_url(__FILE__);
+function sgMailAddStyles() {
+    wp_enqueue_style('sendgrid-styles', plugin_dir_url(__FILE__) . '/style.css');
+}
+add_action('admin_enqueue_scripts', 'sgMailAddStyles');
 ?>
