81bd2e0a1f742ea56e3e2c74c3baaf57dc42b9db
Devadharshini <143801102+deva16081920@users.noreply.github.com>
2024-09-10 09:50:23 +0530

    this is final on 10.9

--- mail_sending.php
+++ mail_sending.php
@@ -1,35 +1,15 @@
 <?php
-
 /*
 Plugin Name: eMail Sender
 Description: Used to send a mail using SendGrid.
 Author: DD
 */
-echo'
-<div id="unique-plugin-section">';
 
 require_once __DIR__ . '/vendor/autoload.php';
 use SendGrid\Mail\Mail;
 
-function display_ing(){
-    echo'
-    <div class="container">
-        <div class="extra">
 
-            <div class="content">
-                <h2><b><i>Great ease of use</b></h2>
-                <h2><b><i> and deliverability</b></h2>
-            </div>
-           
-            <p>This tool offers great ease of use and exceptional deliverability, providing an effective and streamlined way to communicate with our product support team.</p>
-        </div>
-    </div>
-    ';
-    // <img src="' . plugins_url('images/mail.png', __FILE__).'" class="imag"/>
-    // <img src="'.plugin_url('images/logo.png',__FILE__).'"  class="sendgrid-logo" />
-}
 function email_sender_form() {
-    display_ing();
 
     if (isset($_POST['send_email'])) {
         
@@ -67,10 +47,12 @@ function email_sender_form() {
             echo '<div class="notice notice-error"><p>Failed to send the email. Exception: ' . $e->getMessage() . '</p></div>';
         }
     }
-
+//     <div class="image">
+//     <img src="images/pr.avif" alt="">
+//      </div>
     echo '
-  
     <div class="container">
+    
     <form method="post" action="" class="overall">
         <p class="heading"> Email Sender </p>
         <p>
@@ -103,9 +85,7 @@ function sender_menu() {
 }
 add_action('admin_menu', 'sender_menu');
 function sgMailAddStyles() {
-    wp_enqueue_style('sendgrid-styles', plugin_dir_url(__FILE__) . '/style.css');
+    wp_enqueue_style('sendgrid-styles', plugin_dir_url(__FILE__) . 'style.css');
 }
 add_action('admin_enqueue_scripts', 'sgMailAddStyles');
-
-echo'</div>';
 ?>
